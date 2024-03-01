1d6cc06c9c0f8b34932ce6fd01794601b9611fe5
Eduardo da Silva Dörr <edudorr@hotmail.com>
2024-03-01 19:48:56 -0300

    feat: add transaction controller

---
+++ YourGoals.API/Controllers/TransactionsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using MediatR;
+
+using YourGoals.Core.Results;
+using YourGoals.API.Extensions;
+using YourGoals.Application.Transactions.GetTransaction;
+using YourGoals.Application.Transactions.GetTransactions;
+using YourGoals.Application.Transactions.CreateTransaction;
+using YourGoals.Application.Transactions.DeleteTransaction;
+
+namespace YourGoals.API.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class TransactionsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public TransactionsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll([FromQuery] GetTransactionsQuery query)
+    {
+        var result = await _mediator.Send(query);
+
+        return result.Match(
+        onSuccess: Ok,
+        onFailure: this.GetResult);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var query = new GetTransactionQuery(id);
+
+        var result = await _mediator.Send(query);
+
+        return result.Match(
+        onSuccess: Ok,
+        onFailure: this.GetResult);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateTransactionCommand command)
+    {
+        var result = await _mediator.Send(command);
+
+        return result.Match(
+        onSuccess: value => CreatedAtAction(nameof(GetById), new { id = value }, command),
+        onFailure: this.GetResult);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var command = new DeleteTransactionCommand(id);
+
+        var result = await _mediator.Send(command);
+
+        return result.Match(
+        onSuccess: NoContent,
+        onFailure: this.GetResult);
+    }
+}
